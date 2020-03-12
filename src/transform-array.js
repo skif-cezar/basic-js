ccd7f7a8d5d7f6a29a18afabbabff2ce22f55d0c
skif-cezar <skif.cezar@gmail.com>
2020-03-12 22:09:40 +0300

    transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(arr.length == 0){
+        return [];
+    }else if(arr.length == undefined){
+        throw Error;
+    }
+
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == '--discard-next'){
+            if(i < arr.length - 1){
+                i++;
+            }
+        }else if(arr[i] == '--discard-prev'){
+            if(i > 0){
+                newArr.pop();
+            }
+        }else if(arr[i] == '--double-next'){
+            if(i < arr.length - 1){
+                newArr.push(arr[i + 1]);
+            }
+        }else if(arr[i] == '--double-prev'){
+            if(i > 0){
+                newArr.push(arr[i - 1]);
+            }
+        }else newArr.push(arr[i]);
+    }
+
+    return newArr;
 };
