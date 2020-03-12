120a59d0aacffb679b4d125eb0cbb30268f9061f
skif-cezar <skif.cezar@gmail.com>
2020-03-12 15:10:40 +0300

    carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let valueActivity = parseFloat(sampleActivity);
+
+  if(typeof sampleActivity != 'string'){
+    return false;
+  }
+
+  if(valueActivity > MODERN_ACTIVITY || valueActivity <= 0){
+    return false
+  }
+
+  if(isNaN(valueActivity)) return false;
+
+  let c = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / valueActivity) / c);
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  var count = 0;
+  for(let i of matrix){
+    for(let j of i){
+      if(j === '^^') count++;
+    }
+  }
+  return count;
+
 };
