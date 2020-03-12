7b2f8764611f629ca7f62423c1dfc82d3296ce72
skif-cezar <skif.cezar@gmail.com>
2020-03-12 16:29:50 +0300

    dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arr = [];
+  let nameTeam = [];
+  if (!Array.isArray(members)) return false;
+  for(let i = 0; i < members.length; i++){
+    if(typeof (members[i]) == 'string'){
+      arr.push(members[i].toLocaleUpperCase().replace(/\s/g, ''));
+    };
+  }
+
+  arr.sort();
+  for(let i = 0; i < arr.length; i++){
+    nameTeam.push(arr[i][0]);
+  }
+  return (nameTeam.join(''));
 };
